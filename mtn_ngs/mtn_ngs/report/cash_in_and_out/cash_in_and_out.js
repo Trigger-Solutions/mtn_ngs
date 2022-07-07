97ff7e156c555b64ecc03e6eeef0a98b2eb81b08
Trigger-Solutions <trigger.solutions.eg@gmail.com>
2022-07-07 07:14:46 +0000

    first commit

---
+++ mtn_ngs/mtn_ngs/report/cash_in_and_out/cash_in_and_out.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2016, Trigger Solutions pvt. ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+frappe.query_reports["Cash In and Out"] = {
+	"filters": [
+		{
+			fieldname: "fiscal_year",
+			label: __("Fiscal Year"),
+			fieldtype: "Link",
+			options: "Fiscal Year",
+			default: frappe.sys_defaults.fiscal_year
+		},
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+		},
+	]
+}
---
+++ mtn_ngs/mtn_ngs/report/cash_in_and_out/cash_in_and_out.py
@@ -0,0 +1,157 @@
+# Copyright (c) 2013, Trigger Solutions pvt. ltd. and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from erpnext.accounts.report.financial_statements import get_period_list
+from console import console
+
+def execute(filters=None):
+	columns, data = [], []
+	period_list = get_period_list(filters.fiscal_year, filters.fiscal_year, '', '','Fiscal Year', "Monthly")
+	data = get_account_type()
+	get_opening(data,period_list[0]["year_end_date"])
+	get_monthes(data,period_list)
+	columns = get_columns(period_list)
+	return columns, data
+
+
+def get_account_type():
+	return( frappe.db.sql(f"""
+		Select
+			name
+		from
+			`tabAccount`
+		where
+			account_type in ("Cash","Bank")
+	""", as_dict=1))
+
+def get_opening(accounts,date):
+	for account in accounts:
+		opening = frappe.db.sql(f"""
+						Select 
+							COALESCE(SUM(debit)-SUM(credit), 0 ) as 'opening'
+						from 
+							`tabGL Entry` 
+						where 
+							account = '{account["name"]}'
+						and 
+							is_cancelled = 0
+						and 
+							posting_date between '2000-01-01'
+						and '{frappe.utils.add_days(date,-1)}';
+					""", as_dict=1)
+		account.update(opening[0])
+	return accounts
+
+
+def get_monthes(accounts,period_list):
+	for account in accounts:
+		for period in period_list:
+			monthes = frappe.db.sql(f"""
+							Select 
+								COALESCE(SUM(debit), 0)  as '{period.key + "_debit"}' ,
+								COALESCE(SUM(credit), 0)  as '{period.key + "_credit"}' ,
+								COALESCE(SUM(debit)-SUM(credit), 0 ) as '{period.key + "_balance"}' 
+							from 
+								`tabGL Entry` 
+							where 
+								account = '{account.name}'
+							and 
+								is_cancelled = 0
+							and 
+								posting_date between '{period['from_date']}'
+							and '{period['to_date']}';
+						""", as_dict=1)
+			account.update(monthes[0])
+		total_monthes = frappe.db.sql(f"""
+				Select 
+					COALESCE(SUM(debit), 0) as 'total_debit' ,
+					COALESCE(SUM(credit), 0) as 'total_credit' ,
+					COALESCE(SUM(debit)-SUM(credit), 0 ) as 'total_balance' 
+				from 
+					`tabGL Entry` 
+				where 
+					account = '{account.name}'
+				and 
+					is_cancelled = 0
+				and 
+					posting_date between '{period_list[0]['year_start_date']}'
+				and '{period_list[0]['year_end_date']}';
+			""", as_dict=1)
+		account.update(total_monthes[0])
+		
+	return accounts
+
+def get_columns(period_list):
+	columns = [
+		{
+			"fieldname": "name",
+			"label": _("Account"),
+			"fieldtype": "Link",
+			"options": "Account",
+			"width": 300
+		},
+		{
+			"fieldname": "opening",
+			"label": _("Opening Balance"),
+			"fieldtype": "Currency",
+			"width": 300,
+			"options": "currency"
+		}
+	]
+
+	for period in period_list:
+		columns.append({
+			"fieldname": period.key + "_debit",
+			"label": period.label + "(Debit)",
+			"fieldtype": "Currency",
+			"options": "currency",
+			"width": 150,
+			'default': 0.0
+		})
+		columns.append({
+			"fieldname": period.key +  "_credit",
+			"label": period.label +  "(Credit)",
+			"fieldtype": "Currency",
+			"options": "currency",
+			"width": 150,
+			'default': 0.0
+		})
+		columns.append({
+			"fieldname": period.key + "_balance",
+			"label": period.label + "(Balance)",
+			"fieldtype": "Currency",
+			"options": "currency",
+			"width": 150,
+			'default': 0.0
+		})
+	
+	columns.append({
+			"fieldname":"total_debit",
+			"label":"Total Debit",
+			"fieldtype": "Currency",
+			"options": "currency",
+			"width": 150,
+			'default': 0.0
+		})
+	columns.append({
+		"fieldname":"total_credit",
+		"label": "Total Credit",
+		"fieldtype": "Currency",
+		"options": "currency",
+		"width": 150,
+		'default': 0.0
+	})
+	columns.append({
+		"fieldname": "total_balance",
+		"label": "Total Balance",
+		"fieldtype": "Currency",
+		"options": "currency",
+		"width": 150,
+		'default': 0.0
+	})
+
+	return columns
+
+
